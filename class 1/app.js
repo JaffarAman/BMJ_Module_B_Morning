7122047550f7d2aa916fb5b83ab534c04481bbbe
Jaffar Aman <65033254+JaffarAman@users.noreply.github.com>
2022-01-04 12:01:27 +0500

    first class

---
+++ class 1/app.js
@@ -0,0 +1,152 @@
+
+
+// primitive
+
+// var variable_name = "jaffar";
+// var number = 50;
+// var bool = true / false
+// var isActive = undefined
+// var null = null
+
+// compositive data type
+// Object 
+// Array
+// function
+
+
+
+
+// var myName = "Jaffar"
+// myName  = "Jaffar Aman"
+
+
+// console.log(myName)
+
+
+// var add =  5 * 10
+// console.log(add)
+
+
+// var result = 5 + 10 - (5 * 2) * 0
+// console.log(result)
+
+
+// var name = "jaffar"
+
+// console.log()
+
+
+// var result = 10 % 3
+// console.log(result)
+
+
+// var num1 = parseInt(prompt("enter name"))
+// var num2 = +(prompt("enter name"))
+
+// console.log(num1);
+// console.log(num2);
+
+
+
+
+
+// var age = "20";
+
+// if(age >= 20 || age <=25  ){
+//         console.log(true)
+// }
+// else{
+//     console.log(false)
+
+// }
+
+
+
+
+
+// var arr = ["Jaffar" , "aman"]
+
+// console.log(arr[1])
+
+
+// var obj = {
+//     name : "Jaffar",
+//     age : 20,
+//     gender  : "male"
+// }
+
+
+// for(var key in obj){
+//         console.log( key ,obj[key])
+// }   
+
+
+// pop Push
+// shift unshift
+// splice slice
+
+
+// var arr = [1 , 2 , 3 , 4 , 5]
+
+// arr.push(55 , 10 , 05)
+// arr.unshift(10 , 20)
+
+
+// arr.splice(1 , 2 , "Jaffar" , "aman"  )
+
+// console.log(arr.slice(1 , 4))
+
+// console.log(arr)
+
+
+
+
+// for(var i = 1 ; i <= 10 ; i++){
+
+//     console.log("j")
+
+//     for(var j = 0 ; j < 5 ; j++){
+//         console.log("a")
+//     }
+
+
+// }
+
+
+// var arr = [ "talha", "ameen" ,"umeR"]
+
+// var flag = false
+
+// for (var i = 0; i < arr.length; i++) {
+
+//     if(arr[i]  === "umer"){
+//         flag = true
+//         console.log("yes")
+//         break
+        
+//     }
+
+// }
+// if(!flag){
+//     console.log("no")
+// }
+
+
+
+// function foo (  ) {
+
+// }
+
+// function foo(a){
+
+// }
+
+// function foo(a , b=5){
+//     return a  + b
+// }
+
+// console.log(foo(5 , 15))
+
+// function foo(e){
+//     console.log(e)
+// }
