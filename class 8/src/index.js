c220c0538a44188394cac10ee6d3c817f4cdd36a
Jaffar Aman <65033254+JaffarAman@users.noreply.github.com>
2022-01-28 12:08:19 +0500

    learning react (class 1)

---
+++ class 8/src/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+ReactDom.render(
+  <>
+    <h1>HELLO WORLD</h1>
+    <h1>HELLO CLASS</h1>
+  </>
+
+  ,
+  document.getElementById("root")
+)
