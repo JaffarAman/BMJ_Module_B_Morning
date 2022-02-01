999d3c4442974af42e9d2c7bd6ad928501e85aac
Jaffar Aman <65033254+JaffarAman@users.noreply.github.com>
2022-02-01 12:13:30 +0500

    React js 2nd Class

---
+++ reactapp/src/Components/ImgComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Image from "../Images/download.jpg"
+
+function Img() {
+    return (
+        <img src={Image} height="400px" width="100%" />
+    )
+}
+
+export default Img
---
+++ reactapp/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+// import React from "react";
+// import ReactDOM from "react-dom"
+
+
+// class MyClass extends React.Component {
+
+//   render() {
+//     return (
+//       <>
+//         <h1>Components</h1>
+//         <h1>HELLO WORLD</h1>
+//       </>)
+//   }
+
+// }
+
+// function MyClass2(){
+//     return (
+//         <h1>HELLO FUNCTION COMPONENT!</h1>
+//     )
+// }
+
+
+// ReactDOM.render(
+//   <>
+//   <MyClass />
+//   <MyClass2 />
+//   <MyClass2 />
+//   <MyClass2 />
+//   </>
+
+//   ,
+//   document.getElementById("root")
+
+// )
+
+
