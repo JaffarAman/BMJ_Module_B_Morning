efbfe49aa0946f19dafc80dcf103ae6f4ca39687
umairkhan1997 <mohumairkhan47@gmail.com>
2022-06-21 12:08:57 +0500

    react native cli

---
+++ React Native/React-Native-Cli/AwesomeProject/App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>HELLO WORLD</Text>
+
+      <Text>REACT NATIVE CLI</Text>
+    </View>
+  )
+}
